0e2b0f3527312ff2d4b689e8138e3081ce081757
root <root@Elise.localdomain>
2022-11-26 00:52:20 -0600

    learned a lot starting over

--- RandomSiteFinder/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Api,Resource,reqparse
-from flask import Flask
-
-app = Flask(__name__)
-api = Api(app)
-
--- RandomSiteFinder/data/__init__.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-from data.email_repo import EmailRepository
-
-sqlengine = create_engine("sqlite://", echo=True, future=True)
-email_repo = EmailRepository(engine=sqlengine)
